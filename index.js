d9767ce91e21729ae11c40b5ff5cca1125cdc87c
DEVAVRAT SINGH <95224039+Devavrt1234@users.noreply.github.com>
2024-10-15 23:22:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to ASHI Node Application !!!!!!");
+  res.send("Welcome to Dev Node Application !!!!!!");
 });
 
 app.get("/info", (req, res) => {
