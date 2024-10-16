6d9ead4943d8e9856a8c1cc5de2d650679abc23e
DEVAVRAT SINGH <95224039+Devavrt1234@users.noreply.github.com>
2024-10-16 10:49:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/app", (req, res) => {
   res.send("Welcome to Dev Node Application !!!!!!");
 });
 
